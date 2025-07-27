36824e76e691d4b24ada46b331c5fcb4a947bd1b
shakiburrashid <shakiburrashid7@gmail.com>
2025-07-27 22:55:35 +0600

    clock format bulk fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ function App() {
       <div className="h-[100vh] m-auto flex justify-center items-center">
         <div className="w-200 h-120 border-2 border-solid border-red-400 flex items-center justify-around">
           <div className="bg-[url(./assets/Clockimage.png)] w-100 h-100 bg-no-repeat bg-size-[100%] relative">
-            <div className=""></div>
-            <div className="minute"></div>
-            <div className="second"></div>
-
+            <div className="hour bg-black w-[3.5%] h-[28%] rounded-xl absolute top-[22%] left-[49.4%] "></div>
+            <div className="bg-black w-[3%] h-[33%] rounded-xl absolute top-[17.8%] left-[49.4%] "></div>
+            <div className="second bg-red-500 w-[2.5%] h-[38%] rounded-xl absolute top-[13%] left-[49.7%]"></div>
+            <div className="bg-black w-[10%] h-[10%] rounded-full absolute top-[44%] left-[46%]"></div>
 
           </div>
           <div className="flex flex-col gap-10 justify-center items-center">
