ad5c57201407d710adfdd36f424f76a5c9102088
shakiburrashid <shakiburrashid7@gmail.com>
2025-07-28 23:25:39 +0600

    update commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -52,35 +52,33 @@ function App() {
 
   return (
     <>
-      {/* <div className="h-[100vh] m-auto flex justify-center items-center"> */}
-      <div className="h-[80vh] flex items-center md:justify-center gap-35 p-5 || max-md:flex-col max-md:w-[100%] max-md:h-[75vh] max-md:gap-8 max-md:items-center max-md:justify-center">
-        <div className="bg-[url(./assets/Clockimage.png)] bg-no-repeat bg-size-[100%]  w-[100vh] h-[100vh]   relative origin-bottom mt-[10%] || max-md:bg-size-[100%] max-md:w-[45vh] max-md:h-[60vh]">
+
+      <div className="h-[80vh] flex items-center md:justify-center gap-35 p-5 || max-md:flex-col max-md:w-[100%] max-md:h-[75vh] max-md:gap-8 max-lg:gap-10 max-lg:justify-center max-lg:items-center max-md:items-center max-md:justify-center">
+        <div className="bg-[url(./assets/Clockimage.png)] bg-no-repeat bg-size-[100%]  w-[100vh] h-[100vh]   relative mt-[10%] origin-bottom || max-md:bg-size-[100%] max-md:w-[45vh] max-md:h-[45vh]">
 
           <div className="w-[100vh] h-[100vh] || max-md:bg-size-[100%] max-md:w-[45vh] max-md:h-[50vh] ">
-            <div className="HOUR bg-black w-[3.5%] h-[28%] rounded-xl absolute top-[22%] left-[49.4%] origin-bottom     || max-md:top-[16.9%] max-md:left-[49%] " style={{ transform: `rotate(${hourRotation}deg)` }}></div>
-            <div className="MINUTE bg-black w-[3%] h-[33%] rounded-xl absolute top-[17.8%] left-[49.4%] origin-bottom   || max-md:top-[11.5%] max-md:left-[50%] " style={{ transform: `rotate(${minuteRoation}deg)` }}></div>
-            <div className="SECOND bg-red-500 w-[2.5%] h-[38%] rounded-xl absolute top-[13%] left-[49.9%] origin-bottom || max-md:top-[5%] max-md:left-[50%] " style={{ transform: `rotate(${secondRotation}deg)` }}></div>
-            <div className="CENTER CIRCLE  bg-black w-[10%] h-[10%] rounded-[100%] absolute top-[44%] left-[46%] || max-md:top-[39%] max-md:left-[48%] max-md:"></div>
+            <div className="HOUR bg-black w-[3.5%] h-[28%] rounded-xl absolute top-[22%] left-[49.4%] origin-bottom  || max-md:top-[22%] max-md:left-[49.5%] " style={{ transform: `rotate(${hourRotation}deg)` }}></div>
+            <div className="MINUTE bg-black w-[3%] h-[33%] rounded-xl absolute top-[17.8%] left-[49.4%] origin-bottom   || max-md:top-[17%] max-md:left-[49.5%]" style={{ transform: `rotate(${minuteRoation}deg)` }}></div>
+            <div className="SECOND bg-red-500 w-[2.5%] h-[38%] rounded-xl absolute top-[13%] left-[49.9%] origin-bottom || max-md:top-[12.5%] max-md:left-[50%] " style={{ transform: `rotate(${secondRotation}deg)` }} ></div>
+            <div className="CENTER CIRCLE  bg-black w-[10%] h-[10%] rounded-full absolute top-[44%] left-[46%] || max-md:top-[45%] max-md:left-[46%] "></div>
           </div>
-
-
         </div>
         <div className="flex flex-col gap-10 justify-center items-center">
-          <div className="grid grid-cols-2 gap-5 items-center text-7xl font-bold || max-md:flex max-md:gap-2 max-md:justify-center max-md:items-center">
-            <h1 className="row-span-2 text-[12rem] || max-md:text-7xl">{hour()}</h1>
-            <h1 className="md:hidden">:</h1>
+          <div className="grid grid-cols-2 gap-5 items-center text-7xl font-bold || max-md:flex max-md:gap-2 max-md:justify-center max-md:items-center max-lg:flex">
+            <h1 className="row-span-2 text-[12rem] || max-md:text-7xl max-lg:text-7xl">{hour()}</h1>
+            <h1 className="lg:hidden">:</h1>
             <h1>{minute()}</h1>
-            <h1 className="md:hidden">:</h1>
+            <h1 className="lg:hidden">:</h1>
             <h1>{seconds()}</h1>
           </div>
           <div>
-            <h1 className="text-5xl font-bold || max-md:text-3xl">{datemonthyear()}</h1>
+            <h1 className="text-5xl font-bold || max-md:text-3xl max-lg:text-3xl">{datemonthyear()}</h1>
           </div>
         </div>
 
       </div>
 
-      {/* </div> */}
+
     </>
   )
 }
